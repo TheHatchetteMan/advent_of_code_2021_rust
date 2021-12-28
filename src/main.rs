7d101138179565c413cebeccbcd1d49b4334c1d5
Hunter Hatchette <hunter.hatchette@dynata.com>
2021-12-28 10:06:47 -0500

    test branch

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use crate::day_four::day_four;
 use crate::day_one::{day_one, day_one_2};
 use crate::day_three::{day_three, day_three_2};
 use crate::day_two::{day_two, day_two_2};
 
+mod day_four;
 mod day_one;
 mod day_three;
 mod day_two;
 mod functions;
+mod day_five;
 
 fn main() {
     day_one();
@@ -14,4 +17,5 @@ fn main() {
     day_two_2();
     day_three();
     day_three_2();
+    day_four();
 }
